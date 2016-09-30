cae289f640cc64d49804192dfc6c183df0ab99b0
Jimmy Briggs <jbriggs@catan.jgalan1.jgraves.com>
2016-09-30 10:50:34 -0400

    WIP DevOps to Ball Fondlers

--- Source/content_script.js
+++ Source/content_script.js
@@ -4,44 +4,50 @@ function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
+  	var child, next;
 
-	var child, next;
+  	switch ( node.nodeType )
+  	{
+  		case 1:  // Element
+  		case 9:  // Document
+  		case 11: // Document fragment
+  			child = node.firstChild;
+  			while ( child )
+  			{
+  				next = child.nextSibling;
+  				walk(child);
+  				child = next;
+  			}
+  			break;
 
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
-
-	switch ( node.nodeType )
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child )
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+  		case 3: // Text node
+              if(node.parentElement.tagName.toLowerCase() != "script") {
+                  handleText(node);
+              }
+  			break;
+  	}
 }
 
 function handleText(textNode)
 {
-	var v = textNode.nodeValue;
+  console.log(textNode);
+  var v = textNode.nodeValue;
 
-	v = v.replace(/\bDevOPS\b/g, "Ball Fondlers");
-	v = v.replace(/\bdevops\b/g, "ball fondlers");
-	v = v.replace(/\bDevOps\b/g, "Ball Fondlers");
-	v = v.replace(/\bDevops\b/g, "Ball fondlers");
-  v = v.replace(/\bdevOps\b/g, "ball Fondlers");
-
-
+  // Deal with the easy case
+  v = v.replace(/\b(D|d)ev (O|o)ps/g, function(match, p1, p2, offset, string) {
+    // t - 7 = m
+    // c - 1 = b
+    m = String.fromCharCode(p1.charCodeAt(0) - 7);
+    b = String.fromCharCode(p2.charCodeAt(0) - 1);
+    return m + "all " + b + "ondlers";
+  });
+  // Get the corner cases
+  if(v.match(/devops/i)) {
+    // If we're not talking about weather
+    v = v.replace(/(D|d)evOps/gi, function(match, p1, offset, string) {
+      // c - 1 = b
+      return "Ball Fondlers";
+    });
+  }
 	textNode.nodeValue = v;
 }
