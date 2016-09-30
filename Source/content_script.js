c23836c8c8060cca38c8be04d6aea066b77f2674
Jimmy Briggs <jbriggs@catan.jgalan1.jgraves.com>
2016-09-30 10:23:57 -0400

    changed the cloud to butt => devops to ball fondlers

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,24 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
-	
+
 	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
 	    || node.classList.indexOf('ace_editor') > -1) {
 		return;
 	}
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,16 +32,16 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bDevOPS\b/g, "Ball Fondlers");
+	v = v.replace(/\bdevops\b/g, "ball fondlers");
+	v = v.replace(/\bDevOps\b/g, "Ball Fondlers");
+	v = v.replace(/\bDevops\b/g, "Ball fondlers");
+  v = v.replace(/\bdevOps\b/g, "ball Fondlers");
+
+
 	textNode.nodeValue = v;
 }
-
-
